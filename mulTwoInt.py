42eed93a3f1915e9a83aef4df6e90eb64f27a797
sonybelone <sonymekeme@gmail.com>
2020-03-09 14:34:55 +0400

    changement de script

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -1,10 +1,11 @@
 import sys
 
-def mul (a,b):
+def mul(a,b):
 	return a*b
-
-if __name__== "__main__":
-
-	a = int(sys.argv[1])
-	b = int(sys.argv[2])
-	print(mul(a,b))
+if __name__=="__main__":
+	if len(sys.argv)>3:
+		print( "veuillez entrez uniquement deux nombres s'il vous plait")
+	elif len(sys.argv)==3:
+		a= int(sys.argv[1])
+		b= int(sys.argv[2])
+		print(mul(a,b))
