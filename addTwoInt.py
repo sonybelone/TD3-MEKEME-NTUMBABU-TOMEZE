c2a2a7bc8d74005001d9535efe7fd733b03d6356
Ntumbabu <tshiteyababci@gmail.com>
2020-03-09 14:35:49 +0400

    nouvelle modification

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,12 +1,27 @@
 import sys
 
+
 def  add (a, b):
 	return a+b
 if __name__=="__main__":
-	print(add(2,4))
-	a = sys.argv[1]
-	b = sys.argv[2]
-	print(a)
-	print(b)
 
+	if len(sys.argv) > 3:
+		print ("falt")
+	elif len(sys.argv)==3:
+		a = sys.argv[1]
+		b = sys.argv[2]
+		print(sys.argv)
+		print(add(a,b))
+
+
+
+#	try :
+#		x=int(x)
+#	except:
+#		 print("Impossible d'insere plus de deux valeur")
+
+		#print("le contenue ne doit pas depasse plus de 3")
+
+#	except Exception as e:
+#		print(e)
 
