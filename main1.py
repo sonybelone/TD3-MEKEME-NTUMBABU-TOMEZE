91d9811db82004d0393dfc192cc8f9befdf61bd0
Ntumbabu <tshiteyababci@gmail.com>
2020-03-11 16:11:46 +0400

    debut du nouveau programme

--- main1.py
+++ main1.py
@@ -1,1 +1,35 @@
-print("hello world")
+import sys
+
+
+def  add (a, b):
+	return a+b
+#x="oui"
+#y="non"
+x, y = input("vous le vous additione deux variable ? : ")
+if x == "oui" :
+	print("ok")
+
+elif y == "non":
+	print("pas d'accord")
+
+
+
+
+
+
+#if __name__=="__main__":
+#
+#	if len(sys.argv) > 3:
+#		print ("falt")
+#	elif len(sys.argv)==3:
+#		a = sys.argv[1]
+#		b = sys.argv[2]
+#		print(add(a,b))
+#	elif len(sys.argv)==2:
+#		a= int(sys.argv[1])
+#		b= int(input("inserer une deuxieme valeur: "))
+#		print(add(a,b))
+#	else:
+#		a= int(input("inserer la premier: "))
+#		b= int(input("inserer la deuxieme valeur: "))
+#		print(add(a,b))
